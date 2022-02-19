1821b030002178a529cf068ce93a4236f11e08d0
Shreyash <shreyashkakde69@gmail.com>
2022-02-19 16:40:02 +0530

    formated all code

--- Challenge 1/static/js/script.js
+++ Challenge 1/static/js/script.js
@@ -1,19 +1,20 @@
 // Challenge 1: Your Age in Days
 
-function ageInDays(){
+function ageInDays() {
     var birthYear = prompt("What is your Birthyear...Good Firend");
-    var ageInDays=(2022- birthYear) * 365;
+    var ageInDays = (2022 - birthYear) * 365;
     console.log(ageInDays);
     var h1 = document.createElement("h1");
-    var textAnswer = document.createTextNode("Your are :"+ageInDays+" Days old");
-    h1.setAttribute('id','ageInDays');
+    var textAnswer = document.createTextNode("Your are :" + ageInDays + " Days old");
+    h1.setAttribute('id', 'ageInDays');
     h1.appendChild(textAnswer);
     document.getElementById("flex-box-result").appendChild(h1);
-    
+
 }
 
-function reset(){
+function reset() {
     document.getElementById('ageInDays').remove();
 }
+
 
 
