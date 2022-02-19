a4d723a9e28307701b012799766844e308130928
Shreyash <shreyashkakde69@gmail.com>
2022-02-19 16:34:07 +0530

    Learning Js Day 2 Challenge 1 Complete

--- Challenge 1/static/js/script.js
+++ Challenge 1/static/js/script.js
@@ -1,2 +1,19 @@
-// alert("hello world");
-// console.log("Hello");
+// Challenge 1: Your Age in Days
+
+function ageInDays(){
+    var birthYear = prompt("What is your Birthyear...Good Firend");
+    var ageInDays=(2022- birthYear) * 365;
+    console.log(ageInDays);
+    var h1 = document.createElement("h1");
+    var textAnswer = document.createTextNode("Your are :"+ageInDays+" Days old");
+    h1.setAttribute('id','ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById("flex-box-result").appendChild(h1);
+    
+}
+
+function reset(){
+    document.getElementById('ageInDays').remove();
+}
+
+
