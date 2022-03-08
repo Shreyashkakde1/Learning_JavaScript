9340898d77e52f0b3686fa8fb03cf3bcda825727
Shreyash <shreyashkakde69@gmail.com>
2022-03-08 14:47:35 +0530

    challenge 4 done

--- Challenge/static/js/script.js
+++ Challenge/static/js/script.js
@@ -86,23 +86,85 @@ function rpsForntEnd(humanImageChoice, botImageChoice, finalMessage) {
     scissors: document.getElementById("scissors").src,
   };
 
-    // To Remove all the images
-    document.getElementById('rock').remove();
-    document.getElementById('paper').remove();
-    document.getElementById('scissors').remove();
+  // To Remove all the images
+  document.getElementById("rock").remove();
+  document.getElementById("paper").remove();
+  document.getElementById("scissors").remove();
 
-    var humanDiv = document.createElement('div');
-    var botDiv = document.createElement('div');
-    var messageDiv = document.createElement('div');
+  var humanDiv = document.createElement("div");
+  var botDiv = document.createElement("div");
+  var messageDiv = document.createElement("div");
 
-    humanDiv.innerHTML=" <img src='"+imageDatabase[humanImageChoice] + "' hight=150px width=150px style='box-shadow: 0px 10px 50px rgba(37,50,233,1);' >"
-    messageDiv.innerHTML= "<h1 style='color:"+finalMessage['color'] + "; font-size:60px ; padding: 30px; '> "+ finalMessage['message'] + "</h1>"
-    botDiv.innerHTML=" <img src='"+imageDatabase[botImageChoice] + "' hight=150px width=150px style='box-shadow: 0px 10px 50px rgba(243,38,24,1);' >"
-   
+  humanDiv.innerHTML =
+    " <img src='" +
+    imageDatabase[humanImageChoice] +
+    "' hight=150px width=150px style='box-shadow: 0px 10px 50px rgba(37,50,233,1);' >";
+  messageDiv.innerHTML =
+    "<h1 style='color:" +
+    finalMessage["color"] +
+    "; font-size:60px ; padding: 30px; '> " +
+    finalMessage["message"] +
+    "</h1>";
+  botDiv.innerHTML =
+    " <img src='" +
+    imageDatabase[botImageChoice] +
+    "' hight=150px width=150px style='box-shadow: 0px 10px 50px rgba(243,38,24,1);' >";
 
-    document.getElementById('flex-box-rps-div').appendChild(humanDiv);
-    document.getElementById('flex-box-rps-div').appendChild(messageDiv);
-    document.getElementById('flex-box-rps-div').appendChild(botDiv);
-    
+  document.getElementById("flex-box-rps-div").appendChild(humanDiv);
+  document.getElementById("flex-box-rps-div").appendChild(messageDiv);
+  document.getElementById("flex-box-rps-div").appendChild(botDiv);
+}
 
+// Challenge 4: Change the color of all buttons
+
+var all_buttons = document.getElementsByTagName("button");
+console.log(all_buttons);
+
+var copyAllButtons = [];
+for (var i = 0; i < all_buttons.length; i++) {
+  copyAllButtons.push(all_buttons[i].classList[1]);
+}
+
+console.log(copyAllButtons);
+
+function buttonColorChange(buttonTingy){
+    // console.log(buttonTingy.value);
+    if(buttonTingy.value==='red'){
+      buttonsRed();
+    }else if(buttonTingy.value === 'green'){
+      buttonGreen();
+    } else if(buttonTingy.value === 'reset'){
+      buttonColorRest();
+    } else if(buttonTingy.value === 'random'){
+      randomColors();
+    }
+}
+function buttonsRed(){
+  for(var i=0;i<all_buttons.length;i++){
+    all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+    all_buttons[i].classList.add('btn-danger');
+  }
+}
+
+function buttonGreen(){
+  for(var i=0;i<all_buttons.length;i++){
+    all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+    all_buttons[i].classList.add('btn-success');
+  }
+}
+
+function buttonColorRest(){
+    for(var i=0;i<all_buttons.length;i++){
+      all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+      all_buttons[i].classList.add(copyAllButtons[i]);
+    }
+}
+
+function randomColors(){
+  var choices = ['btn-primary','btn-danger','btn-success','btn-warning'];
+  for(var i=0;i<all_buttons.length;i++){
+    var randomNumber = Math.floor(Math.random()*4);
+    all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+    all_buttons[i].classList.add(choices[randomNumber]);
+  }
 }
