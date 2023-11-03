830a2fe251dc5f85beb35c5aa5a121403b2fb9df
Shreyash Kakde <shreyashkakde69@gmail.com>
2023-11-03 21:01:08 +0000

    Operations

--- chai_JavaScript/01_basics/03_conversionOperation.js
+++ chai_JavaScript/01_basics/03_conversionOperation.js
@@ -20,7 +20,33 @@ For Boolen
 
 // Converting number to string
 let someNumber = 99;
-console.log("someNumber: "+typeof someNumber);
+// console.log("someNumber: "+typeof someNumber);
 let newNumber = String(someNumber)
-console.log("newNumber: "+typeof newNumber);
+// console.log("newNumber: "+typeof newNumber);
+
+
+// ******************* Operations *******************
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "Hello ";
+let str2 = "Shreysh"
+let str3 = str1+str2;
+console.log(str3);
+
+// console.log("1"+2); // 12
+// console.log(1+"2"); // 12
+// console.log("1"+2+2); // 122
+// console.log(1+2+"2"); // 32
+
+let num1,num2,num3;
+
+num1 = num2 = num3 = 50; // Not good way to do this thing
+
+let gameCounter = 100;
+gameCounter++
+console.log(gameCounter);
+
+
 
