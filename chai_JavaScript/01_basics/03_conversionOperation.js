5ad456dbecee648e7b7e210789158d23608e3ddc
Shreyash Kakde <shreyashkakde69@gmail.com>
2023-11-03 20:44:03 +0000

    Conversion of datatypes

---
+++ chai_JavaScript/01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "60";
+
+// console.log(typeof score);
+
+let valueOfNumber = Number(score)
+// console.log(typeof valueOfNumber);
+
+// "33" => 33
+// "33abc" = NaN
+// true => 1; false => 0
+
+/*
+For Boolen
+
+1 => true; 0 => false
+"" => false
+"shreyash" => false
+
+*/
+
+// Converting number to string
+let someNumber = 99;
+console.log("someNumber: "+typeof someNumber);
+let newNumber = String(someNumber)
+console.log("newNumber: "+typeof newNumber);
+
