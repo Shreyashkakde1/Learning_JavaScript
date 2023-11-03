87f0f03b8eef72f5043210efcd2065009d48b398
Shreyash Kakde <shreyashkakde69@gmail.com>
2023-11-03 21:35:02 +0000

    .

---
+++ chai_JavaScript/01_basics/04_comparasions.js
@@ -0,0 +1,9 @@
+// We should avoid this type of comparasions
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+
+// Strict Check (===) checking datatype also
+console.log("2"===2); // false
