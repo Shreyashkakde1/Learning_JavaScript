85485a48cd32ddb616f541ac16b3317beee89197
Shreyash Kakde <shreyashkakde69@gmail.com>
2023-11-03 21:49:26 +0000

    datatype summary

---
+++ chai_JavaScript/01_basics/dataTypes-summary.js
@@ -0,0 +1,20 @@
+// Primitive DataTypes (Cal by Value)
+// 7 Types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Non Premitive Datatype (Reference)
+// Arrays, Objects, Functions
+
+// Array
+const heros = ["shaktiman","naagraj","doga"];
+
+// Objects
+let person ={
+    name : "shreyash",
+    age : 21,
+    gender: "M"
+};
+
+// Functions
+const myFunction = function(){
+    console.log("Hello World");
+}
