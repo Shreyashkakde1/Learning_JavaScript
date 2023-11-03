4416a45a5440b023496de4f9a52c9f622b67d0f8
Shreyash Kakde <shreyashkakde69@gmail.com>
2023-11-03 20:21:55 +0000

    dataTypes

---
+++ chai_JavaScript/01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1444556;
+let accountEmail = "Shreyashkakde20@gmail.com"; 
+var accountPassword = 'somePassword'
+accountCity = "Banglore";
+let accountState;
+
+
+accountEmail = "sk@sk.com"
+accountPassword = "newPassword"
+
+console.table([accountId, accountEmail, accountCity, accountPassword, accountState]);
---
+++ chai_JavaScript/01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict" // treat all js code as a newer version
+
+// nuumber => 2^53 approx
+// bigInt
+// boolean => true or false
+// string = "shreyash"
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object (important)
