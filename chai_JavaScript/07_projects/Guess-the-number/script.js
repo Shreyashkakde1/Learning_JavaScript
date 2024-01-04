dce2fbc0e028b2d8638781cae653ee7ea201ff95
shreyash <shreyashkakde20@gmail.com>
2024-01-04 22:26:16 +0530

    basic projects on async js done

---
+++ chai_JavaScript/07_projects/Guess-the-number/script.js
@@ -0,0 +1,8 @@
+let randomNumber = parseInt(Math.floor(Math.random()*100))
+console.log(randomNumber);
+
+let sumbitButton = document.querySelector('#sbt')
+const userInput = document.querySelector('#guessNumber')
+const guessSlot = document.querySelector('#result')
+const remaning = document.querySelector('#attempts')
+const lowOrHight = document.querySelector('#lowOrHigh')
